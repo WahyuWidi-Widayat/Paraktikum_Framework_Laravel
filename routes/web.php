0a4969b4a7ed94b6eae61193d2ba1416ca1956fc
WahyuWidiWidayat <widayat.wahyuwidi28@gmail.com>
2025-09-09 17:07:49 +0700

    Instalasi Breeze
    menambahakan fitur autentivikasi denagn instalasi brezee

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello, World!';
-});
-
-//Route dengan parameter
-Route::get('/user/{id}', function ($id) {
-    return "User ID: " . $id;
-});
-
-// Route dengan parameter opsional
-Route::get('/user/{name?}', function ($name = 'Guest') {
-    return "Hello, " . $name;
-});
-
-// Route dengan nama
-Route::get('/profile', function () {
-    return "This is the profile page.";
-})->name('profile');
-
-// Menggunakan Route bernama untuk pengalihan
-Route::get('/redirect-to-profile', function () {
-    return redirect()->route('profile');
-});
-
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return "Admin Dashboard";
-    });
-
-    Route::get('profile', function () {
-        return "Admin Profile";
-    });
-});
-
 Route::get('/dashboard', function () {
-    return 'Welcome to your dashboard';
-})->middleware('auth');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('posts', PostController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
