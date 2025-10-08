25c86252d5a3ef8417a30762dee29b966c20d843
WahyuWidiWidayat <widayat.wahyuwidi28@gmail.com>
2025-10-08 17:09:27 +0700

    init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'NaufalSyafiq',
+            'email' => 'openg@example.com',
+        ]);
+        User::factory()->create([
+            'name' => 'MaxSteel',
+            'email' => 'user@example.com',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/product', [ProductController::class, 'index'])->name('product-index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product-create');
+Route::post('/product', [ProductController::class, 'store'])->name('product-store');
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product-edit');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product-update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,7 +18,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
